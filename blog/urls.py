d2a0efee384239bd7e19d699bf39ffde41bdcb6b
Iason Saganas <iasonsaga@gmail.com>
2022-06-03 13:59:10 +0200

    Beschreibung

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path("fitting_interface",views.fitting_interface,name="fitting_interface")
 ]
--- blog/views.py
+++ blog/views.py
@@ -41,3 +41,6 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+def fitting_interface(request):
+    return render(request,"blog/base_fitting_interface.html")
+
